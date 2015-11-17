1ba4a650dfc00bd142b28d7def81aa846a15c7e9
cdi408 <cdi408@pltz851.AFPA-PARIS-DI>
2015-11-17 10:53:21 +0100

    Création du projet

---
+++ monPremierGit/src/essai/AppliMain.java
@@ -0,0 +1,11 @@
+package essai;
+
+
+public class AppliMain {
+    public static void main(String []args ){
+        int i = 3;
+        int k = 18;
+        System.out.println("i*k="+(i*k));
+                
+    }
+}
